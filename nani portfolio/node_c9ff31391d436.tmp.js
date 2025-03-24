0f5481303dbf9518e19f6026e772b0ebe785d3e7
nanishgirishek <nanishnanish6@gmail.com>
2025-03-24 12:05:21 -0700

    nani 1cmt

---
+++ nani portfolio/node_c9ff31391d436.tmp.js
@@ -0,0 +1,39 @@
+body {
+    background-color: #0A192F;
+    color: white;
+    font-family: Arial, sans-serif;
+}
+
+.hero {
+    padding: 100px 0;
+    text-align: center;
+}
+
+.hero h1, .hero h2, .hero h3 {
+    margin-bottom: 10px;
+}
+
+.social-icons a {
+    color: white;
+    margin: 0 10px;
+    font-size: 24px;
+    text-decoration: none;
+}
+
+.social-icons a:hover {
+    color: #007bff;
+}
+.container{
+
+}
+.img{
+    flex-wrap: wrap;
+    margin: 3%;
+    padding: ;
+
+    border-radius: 50%;
+    text-shadow: #007bff 20px;
+    width: 350px;
+    height: 450px;
+    text-align: start;
+}
